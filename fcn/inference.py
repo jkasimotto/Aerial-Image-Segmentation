61d8a622af324ea92e5eee4d273c285f6dba9e2c
Angus Clark <acla6624@uni.sydney.edu.au>
2022-08-27 21:25:12 +0000

    * Moved inference.py file to fcn folder * Added -i argument to inference.py to specify a single image in the directory to run on
    Former-commit-id: 4fab1bc4904c3c76fe807962a84f9570882b4fba

--- fcn/inference.py
+++ fcn/inference.py
@@ -9,11 +9,11 @@ import torchvision.transforms.functional as f
 import matplotlib.pyplot as plt
 
 
-def show(imgs):
-    if not isinstance(imgs, list):
-        imgs = [imgs]
-    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
+def show(images):
+    if not isinstance(images, list):
+        images = [images]
+    fig, axs = plt.subplots(ncols=len(images), squeeze=False)
+    for i, img in enumerate(images):
         img = img.detach()
         img = f.to_pil_image(img)
         axs[0, i].imshow(np.asarray(img))
@@ -25,7 +25,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("model", help="checkpoint file for pretrained model")
     parser.add_argument("image_dir", help="path to directory containing images to run through the model")
+    parser.add_argument("-i", "--index", type=int)
     args = parser.parse_args()
+
+    start, end = 0, args.index
+    if args.index is not None:
+        start = args.index - 1
+        if args.index == 0:
+            start, end = 0, 1
 
     model = torch.load(args.model)
 
@@ -39,7 +46,7 @@ def main():
                 child.running_var = None
 
     masked_images = []
-    for filename in os.listdir(args.image_dir):
+    for filename in os.listdir(args.image_dir)[start: end]:
         # Get image and convert to required format
         img_path = os.path.join(args.image_dir, filename)
         image = read_image(img_path, mode=ImageReadMode.RGB)
