366321759d235df8f353d184e66bb6ee069fa4c9
julianotto <julian.kasimir.otto@gmail.com>
2022-08-29 05:50:24 +1000

    Stylistic fixes
    Removing global variables and using HYPER_PARAMS dictionary to be consistent with project
    
    
    Former-commit-id: b4cd657ed3c6549974266141c69cbf22342ee497

--- unet/train.py
+++ unet/train.py
@@ -14,27 +14,7 @@ from utils import (
     test
 )
 
-LEARNING_RATE = 1e-4
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 2
-NUM_EPOCHS = 1
-NUM_WORKERS = 2
-IMAGE_HEIGHT = 512
-IMAGE_WIDTH = 512
-PIN_MEMORY = True
-LOAD_MODEL = False
-
-# 1.  Directories are different
-# 2. Can train on C
-
-DIR = "/home/julianotto/workspace/comp3888_w08_02/"
-TRAIN_IMG_DIR = DIR + "local/data/synthetic/train/images_tiled"
-TRAIN_MASK_DIR = DIR + "local/data/synthetic/train/masks_tiled"
-VAL_IMG_DIR = DIR + "local/data/synthetic/val/images_tiled"
-VAL_MASK_DIR = DIR + "local/data/synthetic/val/masks_tiled"
-
-
-def train(model, criterion, optimizer, dataloader, scaler, epochs=1, print_every=10):
+def train(model, criterion, optimizer, dataloader, scaler, device, epochs=1, print_every=10):
     print("\n==================")
     print("| Training Model |")
     print("==================\n")
@@ -45,8 +25,8 @@ def train(model, criterion, optimizer, dataloader, scaler, epochs=1, print_every
     for epoch in range(epochs):
         running_loss = 0
         for batch, (images, labels) in enumerate(dataloader):
-            images = images.to(device=DEVICE)
-            labels = labels.float().unsqueeze(1).to(device=DEVICE) # Add a channel dimension
+            images = images.to(device=device)
+            labels = labels.float().unsqueeze(1).to(device=device) # Add a channel dimension
             # forward
             with torch.cuda.amp.autocast(): # Allows mixed precision operations for forward pass.
                 predictions = model(images)
@@ -62,65 +42,106 @@ def train(model, criterion, optimizer, dataloader, scaler, epochs=1, print_every
                 print(
                     f"Epoch [{epoch + 1}/{epochs}], Step [{batch + 1}/{len(dataloader)}] Loss: {loss.item():.4f}")
         # Print epoch messages
-        print(f"Epochs [{epoch + 1}/{epochs}], Avg Loss: {running_loss / len(dataloader):.4f}")
+        print(
+            f"Epochs [{epoch + 1}/{epochs}], Avg Loss: {running_loss / len(dataloader):.4f}")
         avg_loss_list.append(running_loss / len(dataloader))
         # Save checkpoint each epoch.
         checkpoint = {
             "state_dict": model.state_dict(),
             "optimizer": optimizer.state_dict()
         }
-        save_checkpoint(checkpoint)
+        save_checkpoint(checkpoint, filename=f"epoch-{epoch}-checkpoint.pth")
     # Finish training
     end = time.time()
     print(f"\nTraining took: {end - start:.2f}s")
     return model
 
+
 def main():
+
+    LOAD_MODEL = False
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # ----------------------
+    # DEFINE HYPER PARAMETERS
+    # ----------------------
+
+    HYPER_PARAMS = {
+        'NUM_CLASSES': 2,
+        'BATCH_SIZE': 16,
+        'NUM_WORKERS': 2,
+        'LR': 0.001,
+        'EPOCHS': 15,
+        'PIN_MEMORY': True
+    }
+
+    # ----------------------
+    # CREATE DATASET
+    # ----------------------
+
+    img_dir = '/home/usyd-04a/synthetic/train/images_tiled'
+    mask_dir = '/home/usyd-04a/synthetic/train/masks_tiled/'
+    test_img_dir = '/home/usyd-04a/synthetic/test/images_tiled/'
+    test_mask_dir = '/home/usyd-04a/synthetic/test/masks_tiled/'
+
+    # Augmentations to training set
     train_transforms = A.Compose([
-        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
         A.Rotate(limit=35, p=1),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.1),
         A.Normalize(
-            mean=[0.0,0.0,0.0],
-            std=[1.0,1.0,1.0],
+            mean=[0.0, 0.0, 0.0],
+            std=[1.0, 1.0, 1.0],
             max_pixel_value=255.0,
         ),
         ToTensorV2()])
 
-    val_transforms = A.Compose([
-        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+    # Augmentations to test set
+    test_transforms = A.Compose([
         A.Normalize(
-            mean=[0.0,0.0,0.0],
-            std=[1.0,1.0,1.0],
+            mean=[0.0, 0.0, 0.0],
+            std=[1.0, 1.0, 1.0],
             max_pixel_value=255.0,
         ),
         ToTensorV2()])
-
-    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
-    criterion = nn.BCEWithLogitsLoss() 
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     train_loader, val_loader = get_loaders(
-        TRAIN_IMG_DIR,
-        TRAIN_MASK_DIR,
-        VAL_IMG_DIR,
-        VAL_MASK_DIR,
-        BATCH_SIZE,
+        img_dir,
+        mask_dir,
+        test_img_dir,
+        test_mask_dir,
+        HYPER_PARAMS['BATCH_SIZE'],
         train_transforms,
-        val_transforms,
-        NUM_WORKERS,
-        PIN_MEMORY
+        test_transforms,
+        HYPER_PARAMS['NUM_WORKERS'],
+        HYPER_PARAMS['PIN_MEMORY']
     )
 
+    # ----------------------
+    # DEFINE MODEL
+    # ----------------------
+
+    model = UNET(in_channels=3, out_channels=1).to(device)
+    criterion = nn.BCEWithLogitsLoss()  # binary cross entropy loss
+    optimizer = optim.Adam(model.parameters(), lr=HYPER_PARAMS["LR"])
     # If the forward pass of an operation has float16 inputs, small gradients may not be 
     # representable in float16 and will 'underflow' to 0. Gradient Scaling prevents this.
     # https://pytorch.org/docs/stable/amp.html#gradient-scaling
     scaler = torch.cuda.amp.GradScaler()
-    train(model, criterion, optimizer, train_loader, scaler, NUM_EPOCHS)
+
+    train(model,
+          criterion=criterion,
+          optimizer=optimizer,
+          dataloader=train_loader,
+          scaler=scaler,
+          device=device,
+          epochs=HYPER_PARAMS["EPOCHS"],
+          print_every=30)
+
     save_predictions_as_imgs(
-        val_loader, model, folder="saved_images/", device=DEVICE
+        val_loader, model, folder="saved_images/", device=device
     )
+
 
 # Do this so on Windows there are no issues when using NUM_WORKERS
 if __name__ == "__main__":
--- unet/utils.py
+++ unet/utils.py
@@ -8,11 +8,13 @@ from torchmetrics.functional import jaccard_index, dice
 
 def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
     print("==> Saving Checkpoint")
-    torch.save(state,filename)
+    torch.save(state, filename)
+
 
 def load_checkpoint(checkpoint, model):
     print("==> Loading checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
+
 
 def get_loaders(
     train_dir,
@@ -55,11 +57,12 @@ def get_loaders(
 
     return train_loader, val_loader
 
+
 def save_predictions_as_imgs(
     loader, model, folder="saved_images/", device="cuda"
 ):
     model.eval()
-    for idx, (x,y) in enumerate(loader):
+    for idx, (x, y) in enumerate(loader):
         x = x.to(device=device)
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
@@ -68,8 +71,9 @@ def save_predictions_as_imgs(
             preds, f"{folder}pred_{idx}.png"
         )
         torchvision.utils.save_image(y.unsqueeze(1), f"{folder}/{idx}.png")
-    
+
     model.train()
+
 
 def test(model, dataloader, device, num_classes):
     print("\n=================")
