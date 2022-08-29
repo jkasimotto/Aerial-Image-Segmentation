6f6da1748e512f4fd11cfca64770ce1cba8d8916
Shane Areni <sare9443@uni.sydney.edu.au>
2022-08-29 22:50:03 +1000

    updated instance preprocessing script
    Former-commit-id: e5cc0f18430c2e91b95ef4261128945352027552

--- preprocessing/preprocessing_tiling_instance.py
+++ preprocessing/preprocessing_tiling_instance.py
@@ -17,27 +17,18 @@ def extract_plane_colors(xml_file: str):
     for child in root.findall("object/category0[.='Airplane'].../object_mask_color_rgba"):
         channels = child.text.split(",")
         channels = [int(val) for val in channels]
-        plane_colors.append(channels)
+        plane_colors.append(tuple(channels))
 
     return plane_colors
 
 
-def is_plane_color(px, colors, index):
-    color = colors[index]
-    if px[0] == color[0] and px[1] == color[1] and px[2] == color[2] and px[3] == color[3]:
-        return True
-
-    return False
-
-
-def create_mask(image, colors, index):
-    new_mask = Image.new('L', image.size)
-
+def create_mask(image, colors):
+    new_mask = Image.new('RGB', image.size)
     for x in range(image.size[0]):
         for y in range(image.size[1]):
             px_color = image.getpixel((x, y))
-            if is_plane_color(px_color, colors, index):
-                new_mask.putpixel((x, y), 255)
+            if px_color in colors:
+                new_mask.putpixel((x, y), px_color)
             else:
                 new_mask.putpixel((x, y), 0)
 
@@ -73,19 +64,6 @@ def tile_img(img, tile_size=512):
     return tiles
 
 
-def filter_mask_tiles(masks_tiles):
-    filtered_masks_tiles = []
-    for i in range(len(masks_tiles[0])): # for each tile
-        filtered_masks_tiles.append([])
-        for j in range(len(masks_tiles)): # for each mask
-            img_arr = np.array(masks_tiles[j][i])
-            n_white_pix = np.sum(img_arr > 0)
-            if n_white_pix > 0:
-                filtered_masks_tiles[i].append(masks_tiles[j][i])
-
-    return filtered_masks_tiles
-
-
 def process_files(args):
     start = args['ID'] * args['FILES_PER_WORKER']
     end = start + args['FILES_PER_WORKER']
@@ -102,30 +80,24 @@ def process_files(args):
         img_file = os.path.join(xml_dir.replace("xmls", "images"), filename.replace('.xml', '.png'))
 
         plane_colors = extract_plane_colors(xml_file)
+        img = Image.open(img_file)
+        mask = create_mask(Image.open(mask_file), plane_colors)
 
-        # full_mask = full sized rareplanes mask
-        full_mask = Image.open(mask_file)
-        new_masks = [] # is an array of full sized masks, one for each plane in the image
-        for index, color in enumerate(plane_colors):
-            new_masks.append(create_mask(full_mask, plane_colors, index))
-        og_img = Image.open(img_file)
-
-        og_img_tiles = tile_img(og_img, tile_size=tile_size)
-        new_masks_tiles = [] # is an array of arrays of tiles
-        for mask in new_masks:
-            new_masks_tiles.append(tile_img(mask, tile_size=tile_size))
-
-        # remove blank tiles
-        filtered_masks_tiles = filter_mask_tiles(new_masks_tiles)
+        img_tiles = tile_img(img, tile_size=tile_size)
+        mask_tiles = tile_img(mask, tile_size=tile_size)
 
         # save tiles to file
-        for i, img_tile in enumerate(og_img_tiles):
-            folder_path = os.path.join(output_path, filename.replace('.xml', f'_tile_{i}'))
-            if any(filtered_masks_tiles[i]):
-                os.makedirs(os.path.join(folder_path), exist_ok=True)
-                img_tile.save(os.path.join(folder_path, 'image.png'))
-                for j, mask_tile in enumerate(filtered_masks_tiles[i]):
-                    mask_tile.save(os.path.join(folder_path, f'mask_{j}.png'))
+        os.makedirs(os.path.join(output_path, 'instance_images_tiled'), exist_ok=True)
+        os.makedirs(os.path.join(output_path, 'instance_masks_tiled'), exist_ok=True)
+        for i in range(len(img_tiles)):
+            img_arr = np.array(mask_tiles[i])
+            n_color_pix = np.sum(img_arr > 0)
+            if n_color_pix > 0:
+                tile_name = filename.replace('.xml', f'_tile_{i}.png')
+                img_tiles[i].save(os.path.join(output_path, 'instance_images_tiled', tile_name))
+                tile_name = filename.replace('.xml', f'_tile_{i}_mask.png')
+                mask_tiles[i].save(os.path.join(output_path, 'instance_masks_tiled', tile_name))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -136,7 +108,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     xml_dir = os.path.join(args.path, "xmls")
-    output_path = os.path.join(args.path, "output")
+    output_path = os.path.join(args.path)
 
     num_files = len(os.listdir(xml_dir))
     files_per_worker = num_files // args.num_workers
