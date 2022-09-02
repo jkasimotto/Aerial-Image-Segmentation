b4cb7d03517e25b8846065606dee3a8f5f01810d
julianotto <julian.kasimir.otto@gmail.com>
2022-09-02 14:18:17 +1000

    Ported FCN train and utils to UNET
    Copied code from FCN directory to UNET directory modified to work with UNET model. This adds MIOU and Dice capabilities in addition to saving the best model
    
    
    Former-commit-id: ac0f39c1ca69e66c5d736a965789895517ff63f6

--- unet/train.py
+++ unet/train.py
@@ -1,88 +1,170 @@
+import argparse
+import os
 import time
-import torch
+
 import albumentations as A
-from albumentations.pytorch import ToTensorV2
-from tqdm import tqdm
+import numpy as np
+import torch
 import torch.nn as nn
 import torch.optim as optim
-from model import UNET
-from utils import (
-    load_checkpoint,
-    save_checkpoint,
-    get_loaders,
-    save_predictions_as_imgs,
-    test
-)
+from albumentations.pytorch import ToTensorV2
+from torchmetrics.functional import dice, jaccard_index
+from tqdm import tqdm
 
-def train(model, criterion, optimizer, dataloader, scaler, device, epochs=1, print_every=10):
+from model import UNET
+from utils import (SaveBestModel, get_loaders, save_acc_plot, save_loss_plot,
+                   save_model_2)
+
+
+def train(model, criterion, optimizer, scaler, scheduler, train_loader, test_loader, num_classes, device, epochs=1,
+          print_every=10):
     print("\n==================")
     print("| Training Model |")
     print("==================\n")
 
-    avg_loss_list = []
-    model.train()
     start = time.time()
+
+    save_best_model = SaveBestModel()
+    train_loss, test_loss = [], []
+    iou_acc, dice_acc = [], []
     for epoch in range(epochs):
-        running_loss = 0
-        for batch, (images, labels) in enumerate(dataloader):
-            images = images.to(device=device)
-            labels = labels.float().unsqueeze(1).to(device=device) # Add a channel dimension
-            # forward
-            with torch.cuda.amp.autocast(): # Allows mixed precision operations for forward pass.
-                predictions = model(images)
-                loss = criterion(predictions, labels)
-            # backward
-            optimizer.zero_grad()
-            scaler.scale(loss).backward() # Updates mixed precision weights
-            scaler.step(optimizer) # 
-            scaler.update()
-            running_loss += loss.item()
-            # Print batch messages
-            if (batch + 1) % print_every == 0:
-                print(
-                    f"Epoch [{epoch + 1}/{epochs}], Step [{batch + 1}/{len(dataloader)}] Loss: {loss.item():.4f}")
-        # Print epoch messages
+        print(f"[INFO] Epoch {epoch + 1}")
+
+        train_epoch_loss = train_one_epoch(
+            model, criterion, optimizer, scaler, train_loader, device, print_every)
+        val_epoch_loss, epoch_iou, epoch_dice = test(
+            model, criterion, test_loader, device, num_classes)
+        scheduler.step()
+
+        train_loss.append(train_epoch_loss)
+        test_loss.append(val_epoch_loss)
+        iou_acc.append(epoch_iou)
+        dice_acc.append(epoch_dice)
+
+        save_best_model(val_epoch_loss, epoch, model, optimizer, criterion)
+
         print(
-            f"Epochs [{epoch + 1}/{epochs}], Avg Loss: {running_loss / len(dataloader):.4f}")
-        avg_loss_list.append(running_loss / len(dataloader))
-        # Save checkpoint each epoch.
-        checkpoint = {
-            "state_dict": model.state_dict(),
-            "optimizer": optimizer.state_dict()
-        }
-        save_checkpoint(checkpoint, filename=f"./checkpoints/epoch-{epoch}-checkpoint.pth")
-    # Finish training
+            f"Epochs [{epoch + 1}/{epochs}], Avg Train Loss: {train_epoch_loss:.4f}, Avg Test Loss: {val_epoch_loss:.4f}")
+        print("---\n")
+
     end = time.time()
+
+    save_loss_plot(train_loss, test_loss, 'fcn_loss.png')
+    save_acc_plot(iou_acc, dice_acc, 'fcn_accuracy.png')
+
     print(f"\nTraining took: {end - start:.2f}s")
+
     return model
 
 
-def main():
+def train_one_epoch(model, criterion, optimizer, scaler, dataloader, device, print_every):
+    print('[EPOCH TRAINING]')
+    model.train()
+    running_loss = 0
+    for batch, (images, labels) in enumerate(tqdm(dataloader)):
+        images, labels = images.to(device), labels.to(device)
+        print(images.shape)
+        print(labels.shape)
+        with torch.cuda.amp.autocast():
+            # UNET outputs a single channel, we can remove it.
+            print("PREDICTING!")
+            prediction = model(images).squeeze(1)  # (Batch, H, W)
+            print(prediction.shape)
+            print(labels.shape)
+            loss = criterion(prediction, labels)
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()  # Updates mixed precision weights
+        scaler.step(optimizer)
+        scaler.update()
+        running_loss += loss.item()
 
-    LOAD_MODEL = False
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+        if (batch + 1) % print_every == 0:
+            print(
+                f"Step [{batch + 1}/{len(dataloader)}] Loss: {loss.item():.4f}")
+
+    return running_loss / len(dataloader)
+
+
+def test(model, criterion, dataloader, device, num_classes):
+    print("[VALIDATING]")
+    ious, dice_scores = list(), list()
+    model.eval()
+    start = time.time()
+    running_loss = 0
+    with torch.inference_mode():
+        for images, labels in tqdm(dataloader):
+            images, labels = images.to(device), labels.to(device)
+
+            # UNET outputs a single channel. Squeeze to match labels.
+            prediction = model(images).squeeze(dim=1)
+            loss = criterion(prediction, labels)
+            running_loss += loss.item()
+            prediction = torch.sigmoid(prediction) > 0.5
+            iou = jaccard_index(prediction, labels,
+                                num_classes=num_classes).item()
+            dice_score = dice(prediction, labels,
+                              num_classes=num_classes, ignore_index=0).item()
+            ious.append(iou), dice_scores.append(dice_score)
+
+    end = time.time()
+
+    test_loss = running_loss / len(dataloader)
+    iou_acc = np.mean(ious)
+    dice_acc = np.mean(dice_scores)
+
+    print(f"Accuracy: mIoU= {iou_acc * 100:.3f}%, dice= {dice_acc * 100:.3f}%")
+
+    return test_loss, iou_acc, dice_acc
+
+
+def command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_dir",
+                        help="path to directory containing test and train images")
+    parser.add_argument("-c", "--checkpoint",
+                        help="filename for model checkpoint to be saved as")
+    parser.add_argument("-b", '--batch-size', default=16, type=int,
+                        help="dataloader batch size")
+    parser.add_argument("-lr", "--learning-rate", default=0.001, type=float,
+                        help="learning rate to be applied to the model")
+    parser.add_argument("-e", "--epochs", default=1, type=int,
+                        help="number of epochs to train the model for")
+    parser.add_argument("-w", "--workers", default=2, type=int,
+                        help="number of workers used in the dataloader")
+    parser.add_argument("-n", "--num-classes", default=2, type=int,
+                        help="number of classes for semantic segmentation")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = command_line_args()
+
+    # Use GPU if available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(
+        f'GPU avaliable: {torch.cuda.is_available()} ({torch.cuda.device_count()})')
 
     # ----------------------
     # DEFINE HYPER PARAMETERS
     # ----------------------
 
     HYPER_PARAMS = {
-        'NUM_CLASSES': 2,
-        'BATCH_SIZE': 16,
-        'NUM_WORKERS': 2,
-        'LR': 0.001,
-        'EPOCHS': 15,
+        'NUM_CLASSES': args.num_classes,
+        'BATCH_SIZE': args.batch_size,
+        'NUM_WORKERS': args.workers,
+        'LR': args.learning_rate,
+        'EPOCHS': args.epochs,
         'PIN_MEMORY': True
     }
 
     # ----------------------
     # CREATE DATASET
     # ----------------------
-
-    img_dir = '/home/usyd-04a/synthetic/train/images_tiled'
-    mask_dir = '/home/usyd-04a/synthetic/train/masks_tiled/'
-    test_img_dir = '/home/usyd-04a/synthetic/test/images_tiled/'
-    test_mask_dir = '/home/usyd-04a/synthetic/test/masks_tiled/'
+    img_dir = os.path.join(args.data_dir, 'train/images_tiled')
+    mask_dir = os.path.join(args.data_dir, 'train/masks_tiled')
+    test_img_dir = os.path.join(args.data_dir, 'test/images_tiled')
+    test_mask_dir = os.path.join(args.data_dir, 'test/masks_tiled')
 
     # Augmentations to training set
     train_transforms = A.Compose([
@@ -105,7 +187,7 @@ def main():
         ),
         ToTensorV2()])
 
-    train_loader, val_loader = get_loaders(
+    train_loader, test_loader = get_loaders(
         img_dir,
         mask_dir,
         test_img_dir,
@@ -121,26 +203,36 @@ def main():
     # DEFINE MODEL
     # ----------------------
 
-    model = UNET(in_channels=3, out_channels=1).to(device)
+    device_ids = [i for i in range(torch.cuda.device_count())]
+    model = nn.DataParallel(
+        UNET(in_channels=3, out_channels=1), device_ids=device_ids).to(device)
     criterion = nn.BCEWithLogitsLoss()  # binary cross entropy loss
     optimizer = optim.Adam(model.parameters(), lr=HYPER_PARAMS["LR"])
-    # If the forward pass of an operation has float16 inputs, small gradients may not be 
+    # If the forward pass of an operation has float16 inputs, small gradients may not be
     # representable in float16 and will 'underflow' to 0. Gradient Scaling prevents this.
     # https://pytorch.org/docs/stable/amp.html#gradient-scaling
     scaler = torch.cuda.amp.GradScaler()
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
-    train(model,
-          criterion=criterion,
-          optimizer=optimizer,
-          dataloader=train_loader,
-          scaler=scaler,
-          device=device,
-          epochs=HYPER_PARAMS["EPOCHS"],
-          print_every=30)
+    model = train(model,
+                  criterion=criterion,
+                  optimizer=optimizer,
+                  train_loader=train_loader,
+                  test_loader=test_loader,
+                  scaler=scaler,
+                  scheduler=scheduler,
+                  device=device,
+                  epochs=HYPER_PARAMS["EPOCHS"],
+                  print_every=30,
+                  num_classes=HYPER_PARAMS['NUM_CLASSES'])
 
-    save_predictions_as_imgs(
-        val_loader, model, folder="./saved_images/", device=device
-    )
+    save_model_2(model=model,
+                 epochs=HYPER_PARAMS['EPOCHS'],
+                 optimizer=optimizer,
+                 criterion=criterion,
+                 batch_size=HYPER_PARAMS['BATCH_SIZE'],
+                 lr=HYPER_PARAMS['LR'],
+                 filename='unet_final_epoch.pth')
 
 
 # Do this so on Windows there are no issues when using NUM_WORKERS
