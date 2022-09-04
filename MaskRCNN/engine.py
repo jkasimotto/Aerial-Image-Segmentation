fixing the loss plot, to plot average loss for each epoch
Former-commit-id: 36db8b1f92b751a1d7f0c01f4e30483d8df7e41b

MaskRCNN/engine.py
@@ -25,7 +25,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         lr_scheduler = torch.optim.lr_scheduler.LinearLR(
             optimizer, start_factor=warmup_factor, total_iters=warmup_iters
         )
-
+    temp_loss_val = []
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
@@ -38,7 +38,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
         loss_value = losses_reduced.item()
-        loss_plot.append(loss_value)
+        temp_loss_val.append(loss_value)
 
         if not math.isfinite(loss_value):
             print(f"Loss is {loss_value}, stopping training")
@@ -59,6 +59,11 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+    avg = 0
+    for i in temp_loss_val:
+        avg += i
+    avg = avg/ len(temp_loss_val)
+    loss_plot.append(avg)
 
     return metric_logger
 