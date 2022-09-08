5c722a8fbfa7035dc52c3b266d3279215931a197
Shane Areni <sare9443@uni.sydney.edu.au>
2022-09-08 12:10:42 +1000

    removed COCO code from MaskRCNN
    Former-commit-id: 0677191fe051007b6331519a3c2fee718af38da7

--- MaskRCNN/dataset.py
+++ MaskRCNN/dataset.py
@@ -6,28 +6,28 @@ from torch.utils.data import Dataset
 
 
 class PlanesDataset(Dataset):
-    def __init__(self, img_dir, mask_dir, transforms):
+    def __init__(self, img_dir, mask_dir):
         self.img_dir = img_dir
         self.mask_dir = mask_dir
-        self.transforms = transforms
         # load all image files, sorting them to
         # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(img_dir)))
         self.masks = list(sorted(os.listdir(mask_dir)))
 
     def __getitem__(self, idx):
-        # load images and masks
+        # load image and mask paths
         img_path = os.path.join(self.img_dir, self.imgs[idx])
         mask_path = os.path.join(self.mask_dir, self.masks[idx])
+        # open the original image and conver it to RGB
         img = Image.open(img_path).convert("RGB")
         # note that we haven't converted the mask to RGB,
         # because each color corresponds to a different instance
         # with 0 being background
-        mask = Image.open(mask_path)
+        mask = Image.open(mask_path).convert("L")
         # convert the PIL Image into a numpy array
         mask = np.array(mask)
         # instances are encoded as different colors
-        obj_ids = np.unique(mask)
+        obj_ids = np.unique(mask) # unique elements of mask in ascending order
         # first id is the background, so remove it
         obj_ids = obj_ids[1:]
 
@@ -36,43 +36,25 @@ class PlanesDataset(Dataset):
         masks = mask == obj_ids[:, None, None]
 
         # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
         boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
+        for mask in masks:
+            pos = np.asarray(mask).nonzero() # returns an array of the co-ordinates of pixels that are non-zero
             xmin = np.min(pos[1])
             xmax = np.max(pos[1])
             ymin = np.min(pos[0])
             ymax = np.max(pos[0])
-            if xmax - xmin <= 0:
-                xmax = xmin + 0.001
-            if ymax - ymin <= 0:
-                ymax = ymin + 0.001
             boxes.append([xmin, ymin, xmax, ymax])
-
-        num_objs = len(boxes)
 
         # convert everything into a torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
+        labels = torch.ones(len(obj_ids), dtype=torch.int64) # there is only one class
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-
+        # create the targe dict for training
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = masks
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
 
         return img, target
 
