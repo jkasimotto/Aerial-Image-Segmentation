413841a28cabdece64e47c5e8c7d0c3562a3ee7a
Marcus Archilles <marc9144@uni.sydney.edu.au>
2022-10-08 02:56:32 +0000

    Trial run using yaml for benchmarking
    Former-commit-id: 3c3d9cdb948da9fd97e33059a6b7afdd3d022848

---
+++ DeepLabV3/config/benchmark.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+python DeepLabV3.py config/config_1.yaml
+python DeepLabV3.py config/config_1.yaml
+python DeepLabV3.py config/config_1.yaml
+
+python DeepLabV3.py config/config_2.yaml
+python DeepLabV3.py config/config_2.yaml
+python DeepLabV3.py config/config_2.yaml
+
+python DeepLabV3.py config/config_3.yaml
+python DeepLabV3.py config/config_3.yaml
+python DeepLabV3.py config/config_3.yaml
+
+python DeepLabV3.py config/config_4.yaml
+python DeepLabV3.py config/config_4.yaml
+python DeepLabV3.py config/config_4.yaml
+
+python DeepLabV3.py config/config_5.yaml
+python DeepLabV3.py config/config_5.yaml
+python DeepLabV3.py config/config_5.yaml
+
+python DeepLabV3.py config/config_6.yaml
+python DeepLabV3.py config/config_6.yaml
+python DeepLabV3.py config/config_6.yaml
+
+python DeepLabV3.py config/config_7.yaml
+python DeepLabV3.py config/config_7.yaml
+python DeepLabV3.py config/config_7.yaml
+
+python DeepLabV3.py config/config_8.yaml
+python DeepLabV3.py config/config_8.yaml
+python DeepLabV3.py config/config_8.yaml
