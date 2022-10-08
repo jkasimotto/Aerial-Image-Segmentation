4b6c1d68836355c7a9c1b9c48999c859efd00a0b
Marcus Archilles <marc9144@uni.sydney.edu.au>
2022-10-08 02:53:19 +0000

    Setup DeepLabV3 For benchmarking with AMP Setup DeepLabV3 For benchmarking with Channel Last Setup DeepLabV3 For benchmarking with DDP Created utils file to DeepLabV3
    Former-commit-id: 4d8fcf56cf5b50f004b90e771278f8898c8060a0

--- DeepLabV3/DeepLabV3.py
+++ DeepLabV3/DeepLabV3.py
@@ -1,6 +1,6 @@
-afrom torchmetrics.functional import jaccard_index, dice
+from torchmetrics.functional import jaccard_index, dice
 from torchvision.models.segmentation import deeplabv3_resnet101
-from model_analyser import ModelAnalyzer
+from model_analyser import ModelAnalyser
 from torch.utils.data import DataLoader
 from torch import nn
 from dataset import PlanesDataset
@@ -9,24 +9,31 @@ from tqdm import tqdm
 import time
 import argparse
 import wandb
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 import torch
 import os
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 import torch.multiprocessing as mp
-
+from torch.cuda.amp import autocast, GradScaler
 from utils import (
-    augmentations,
-    my_collate_fn
+    read_config_file,
+    dist_env_setup,
+    dist_process_setup,
+    get_data_loaders,
+    get_memory_format,
+    get_device,
+    get_model,
+    is_main_node
 )
 
 
-def train(model, criterion, optimizer, train_loader, test_loader, analyser, args, rank, num_gpus, use_wandb=False):
-    print("\n==================")
-    print("| Training Model |")
-    print("==================\n")
+def train(model, criterion, optimizer, train_loader, test_loader, analyser, args, scaler=None, rank=None):
+    if is_main_node(rank):
+        print("\n==================")
+        print("| Training Model |")
+        print("==================\n")
+
+    assert args.get('config').get('amp') == (scaler is not None), "Scaler should be not None if AMP is enabled"
 
     # Time how long it takes to train the model
     start = time.time()
@@ -35,23 +42,20 @@ def train(model, criterion, optimizer, train_loader, test_loader, analyser, args
     train_loss, test_loss = [], []
     iou_acc, dice_acc = [], []
 
-    # Training Loop
-    for epoch in range(args.epochs):
-        print(f"[INFO] Epoch {epoch + 1}")
+    # Training loop
+    for epoch in range(args.get('hyper-params').get('epochs')):
+        if is_main_node(rank):
+            print(f"[INFO] Epoch {epoch + 1}")
 
-        # Get training Loss
-        train_epoch_loss = train_one_epoch(model, criterion, optimizer, train_loader, rank)
-        # Get Validation Loss, mIoU and DICE for epoch
-        val_epoch_loss, epoch_iou, epoch_dice = test(model, criterion, test_loader, rank, args)
+        # Epoch training
+        train_epoch_loss = train_one_epoch(model, criterion, optimizer, scaler, train_loader, args, rank)
 
-        # Append data to array for graphing
-        train_loss.append(train_epoch_loss)
-        test_loss.append(val_epoch_loss)
-        iou_acc.append(epoch_iou)
-        dice_acc.append(epoch_dice)
+        # Epoch validation
+        val_epoch_loss, epoch_iou, epoch_dice = test(model, criterion, test_loader, args, rank)
 
-        # Log results to Weights anf Biases
-        if use_wandb:
+
+        # Log results to Weights and Biases
+        if args.get('wandb').get('enabled') and is_main_node(rank):
             wandb.log({
                 'train_loss': train_epoch_loss,
                 "val_loss": val_epoch_loss,
@@ -59,241 +63,188 @@ def train(model, criterion, optimizer, train_loader, test_loader, analyser, args
                 "dice": epoch_dice,
             })
 
+        # Save model performance values
+        train_loss.append(train_epoch_loss)
+        test_loss.append(val_epoch_loss)
+        iou_acc.append(epoch_iou)
+        dice_acc.append(epoch_dice)
+
         # Update best model saved throughout training
-        if rank == 0:
+        if is_main_node(rank):
             analyser.save_best_model(val_epoch_loss, epoch_iou, epoch, model, optimizer, criterion)
 
-        print(
-            f"Epochs [{epoch + 1}/{args.epochs}], Avg Train Loss: {train_epoch_loss:.4f}, Avg Test Loss: {val_epoch_loss:.4f}")
-        print("---\n")
+            print(
+                f"Epochs [{epoch + 1}/{args.get('hyper-params').get('epochs')}], Avg Train Loss: {train_epoch_loss:.4f}, Avg Test Loss: {val_epoch_loss:.4f}")
+            print("---\n")
 
     end = time.time()
 
-    # Create plots for accuracy and loss
-    if rank == 0:
+    # Saving the loss and accuracy plot after training is complete
+    if is_main_node(rank):
         analyser.save_loss_plot(train_loss, test_loss)
         analyser.save_acc_plot(iou_acc, dice_acc)
 
-    print(f"\nTraining took: {end - start:.2f}s")
+        print(f"\nTraining took: {end - start:.2f}s")
 
     return model
 
 
-def train_one_epoch(model, criterion, optimizer, dataloader, rank):
-    print('[EPOCH TRAINING]')
+def train_one_epoch(model, criterion, optimizer, scaler, dataloader, args, rank):
+
+    if is_main_node(rank):
+        print('[EPOCH TRAINING]')
+
+    running_loss = 0
+    use_amp = args.get('config').get('amp')
+    device = get_device(args)
 
     # Set model in training mode
     model.train()
-    running_loss = 0
 
-    # Calculate loss per batch
-    for batch, (images, labels) in enumerate(tqdm(dataloader)):
-        images, labels = images.cuda(rank), labels.cuda(rank)
+    # Calculate loss per batch (don't use tqdm if not main rank)
+    for batch, (images, labels) in enumerate(tqdm(dataloader, disable=not is_main_node(rank))):
+        images = images.to(device, memory_format=get_memory_format(args))
+        labels = labels.to(device, memory_format=get_memory_format(args))
 
-        prediction = model(images)['out']
-        loss = criterion(prediction, labels)
+        with autocast(enabled=use_amp):
+            prediction = model(images)['out']
+            loss = criterion(prediction, labels)
 
         optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
+
+        if use_amp:
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            loss.backward()
+            optimizer.step()
+
         running_loss += loss.item()
 
     # Return average loss for the epoch
     return running_loss / len(dataloader)
 
 
-def test(model, criterion, dataloader, rank, args):
-    print("[VALIDATING]")
+def test(model, criterion, dataloader, args, rank):
+    if is_main_node(rank):
+        print("\n==================")
+        print("| Validating Model |")
+        print("==================\n")
+
+    running_loss = 0
+    ious, dice_scores = list(), list()
+    use_amp = args.get('config').get('amp')
+    num_classes = args.get('config').get('classes')
+    device = get_device(args)
 
     # Set model in evaluation mode
     model.eval()
-    ious, dice_scores = list(), list()
-    start = time.time()
-    running_loss = 0
 
     # Calculate test loss, IoU and Dice coefficient accuracy measures
     with torch.no_grad():
-        for images, labels in tqdm(dataloader):
-            images, labels = images.cuda(rank), labels.cuda(rank)
+        for images, labels in tqdm(dataloader, disable=not is_main_node(rank)):
+            images = images.to(device, memory_format=get_memory_format(args))
+            labels = labels.to(device, memory_format=get_memory_format(args))
 
-            prediction = model(images)['out']
-            loss = criterion(prediction, labels)
+            with autocast(enabled=use_amp):
+                prediction = model(images)['out']
+                loss = criterion(prediction, labels)
+
             running_loss += loss.item()
-            prediction = prediction.softmax(dim=1).argmax(dim=1).squeeze(1)  # (batch_size, w, h)
-
-            labels = labels.argmax(dim=1)  # (batch_size, w, h)
-            iou = jaccard_index(prediction, labels, num_classes=args.num_classes).item()
-            dice_score = dice(prediction, labels, num_classes=args.num_classes, ignore_index=0).item()
+            prediction = prediction.softmax(dim=1).argmax(dim=1).squeeze(1)
+            labels = labels.argmax(dim=1)
+            iou = jaccard_index(prediction, labels, num_classes=num_classes).item()
+            dice_score = dice(prediction, labels, num_classes=num_classes, ignore_index=0).item()
             ious.append(iou), dice_scores.append(dice_score)
-
-    end = time.time()
 
     test_loss = running_loss / len(dataloader)
     iou_acc = np.mean(ious)
     dice_acc = np.mean(dice_scores)
 
-    print(f"Accuracy: mIoU= {iou_acc * 100:.3f}%, dice= {dice_acc * 100:.3f}%")
+    if is_main_node(rank):
+        print(f"Accuracy: mIoU= {iou_acc * 100:.3f}%, dice= {dice_acc * 100:.3f}%")
 
     # Return test loss, IoU and Dice coefficient for the epoch
     return test_loss, iou_acc, dice_acc
 
 
-def command_line_args():
-    parser = argparse.ArgumentParser()
+def training_setup(gpu, args):
 
-    parser.add_argument("data_dir",
-                        help="path to directory containing test and train images")
-    parser.add_argument("checkpoint_dir",
-                        help="directory for model checkpoint to be saved as")
-    parser.add_argument("-r", '--run-name', default="deeplab",
-                        help="used for naming output files")
-    parser.add_argument("-b", '--batch-size', default=16, type=int,
-                        help="dataloader batch size")
-    parser.add_argument("-lr", "--learning-rate", default=0.001, type=float,
-                        help="learning rate to be applied to the model")
-    parser.add_argument("-e", "--epochs", default=1, type=int,
-                        help="number of epochs to train the model for")
-    parser.add_argument("-w", "--workers", default=2, type=int,
-                        help="number of workers used in the dataloader")
-    parser.add_argument("-n", "--num-classes", default=2, type=int,
-                        help="number of classes for semantic segmentation")
-    parser.add_argument("-wandb", "--wandb",
-                        help="use weights and biases to log run", action='store_true')
-    args = parser.parse_args()
-    return args
-
-
-
-def dist_train(rank, args, num_gpus):
-
-    # ----------------------
-    # DEFINE HYPER PARAMETERS
-    # ----------------------
-
-    HYPER_PARAMS = {
-        'num_classes': args.num_classes,
-        'batch_size': args.batch_size,
-        'num_workers': args.workers,
-        'learning_rate': args.learning_rate,
-        'epochs': args.epochs,
-    }
-
-    torch.distributed.init_process_group(
-        backend='nccl',
-        rank=rank,
-        world_size=num_gpus
-    )
     torch.manual_seed(0)
 
+    # Setup process if distributed is enabled
+    rank = None
+    if args.get('config').get('distributed'):
+        rank = dist_process_setup(args, gpu)
 
-    torch.cuda.set_device(rank)
+    hyper_params = args.get('hyper-params')
 
-    # ----------------------
-    # CREATE DATASETda
-    # ----------------------
-
-    # Get train and test directory paths
-    train_img_dir = os.path.join(args.data_dir, 'train/images_tiled')
-    train_mask_dir = os.path.join(args.data_dir, 'train/masks_tiled')
-    test_img_dir = os.path.join(args.data_dir, 'test/images_tiled')
-    test_mask_dir = os.path.join(args.data_dir, 'test/masks_tiled')
-
-    train_transform, test_transform = augmentations()
-
-    # Create object which loads input images and target masks and applies transform
-    train_dataset = PlanesDataset(img_dir=train_img_dir, mask_dir=train_mask_dir,
-                                  num_classes=HYPER_PARAMS['num_classes'], transforms=train_transform)
-    test_dataset = PlanesDataset(img_dir=test_img_dir, mask_dir=test_mask_dir, num_classes=HYPER_PARAMS['num_classes'],
-                                 transforms=test_transform)
-
-    train_sampler = torch.utils.data.distributed.DistributedSampler(
-        train_dataset, num_replicas=num_gpus, rank=rank
-    )
-    test_sampler = torch.utils.data.distributed.DistributedSampler(
-        test_dataset, num_replicas=num_gpus, rank=rank
-    )
-
-    # Pass dataset to dataloader with predefined arguments
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=int(HYPER_PARAMS['batch_size'] / num_gpus),
-        shuffle=False,
-        num_workers=HYPER_PARAMS['num_workers'],
-        drop_last=True,
-        collate_fn=my_collate_fn,
-        pin_memory=True,
-        sampler=train_sampler
-    )
-
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=int(HYPER_PARAMS['batch_size'] / num_gpus),
-        num_workers=HYPER_PARAMS['num_workers'],
-        collate_fn=my_collate_fn,
-        pin_memory=True,
-        sampler=test_sampler
-    )
-
-    # ----------------------
-    # DEFINE MODEL
-    # ----------------------
-
-    # model = DDP(deeplabv3_resnet101(num_classes=HYPER_PARAMS['num_classes']), device_ids=device_ids, output_device=len(device_ids))
-    model = deeplabv3_resnet101(num_classes=HYPER_PARAMS['num_classes']).cuda(rank)
-    model = DDP(model, device_ids=[rank])
-    optimizer = torch.optim.Adam(model.parameters(), lr=HYPER_PARAMS['learning_rate'])
+    # Setup dataloaders, model, criterion and optimiser
+    train_loader, test_loader = get_data_loaders(args, rank)
+    model = get_model(args)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=hyper_params.get('learning-rate'))
     criterion = nn.BCEWithLogitsLoss()
 
-    if args.wandb:
-        wandb.init(project="DeepLabV3", entity="usyd-04a", config=HYPER_PARAMS, dir="./wandb_data")
+    # Configure Weights and Biases
+    if args.get('wandb').get('enabled') and is_main_node(rank):
+        wandb.init(project=args.get('wandb').get('project-name'), entity="usyd-04a", config=args, dir="./wandb_data")
         wandb.watch(model, criterion=criterion)
+        wandb.run.name = args.get('config').get('run')
 
-    analyser = ModelAnalyzer(checkpoint_dir=args.checkpoint_dir, run_name=args.run_name)
+    # Create analyser for saving model checkpoints
+    analyser = ModelAnalyser(
+        checkpoint_dir=args.get('config').get('checkpoint-dir'),
+        run_name=args.get('config').get('run'),
+    )
 
-    # Train model
+    # Setup Gradient Scaler if AMP is enabled
+    scaler = None
+    if args.get('config').get('amp'):
+        scaler = GradScaler()
+
+    # Training loop
     model = train(model=model,
                   criterion=criterion,
                   optimizer=optimizer,
                   train_loader=train_loader,
                   test_loader=test_loader,
+                  scaler=scaler,
                   analyser=analyser,
                   args=args,
-                  rank=rank,
-                  num_gpus=num_gpus,
-                  use_wandb=args.wandb)
+                  rank=rank)
 
-    if rank == 0:
-        # Save model after training
+    if is_main_node(rank):
         analyser.save_model(model=model,
-                            epochs=HYPER_PARAMS['epochs'],
+                            epochs=hyper_params.get('epochs'),
                             optimizer=optimizer,
                             criterion=criterion,
-                            batch_size=HYPER_PARAMS['batch_size'],
-                            lr=HYPER_PARAMS['learning_rate'])
+                            batch_size=hyper_params.get('batch-size'),
+                            lr=hyper_params.get('learning-rate'))
 
-    dist.destroy_process_group()
+    # Clean up distributed process
+    if args.get('config').get('distributed'):
+        dist.destroy_process_group()
 
 
 def main():
     # Load in command line arguments
-    args = command_line_args()
+    args = read_config_file()
 
-    # Use GPU if available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Starting run: {args.get("config").get("run")}\n')
+    print(f'AMP: {args.get("config").get("amp")}')
+    print(f'Channels Last: {args.get("config").get("channels-last")}')
+    print(f'Distributed: {args.get("config").get("distributed")}')
     print(f'GPU avaliable: {torch.cuda.is_available()} ({torch.cuda.device_count()})')
 
-    device_ids = list(range(torch.cuda.device_count()))
-    print(device_ids)
-    num_gpus = len(device_ids)
+    torch.cuda.empty_cache()
 
-    # model = nn.DataParallel(deeplabv3_resnet101(num_classes=HYPER_PARAMS['num_classes']), device_ids=device_ids)
-    # model.to(device)
+    if args.get('config').get('distributed'):
+        dist_env_setup(args)
+        mp.spawn(training_setup, nprocs=args.get("distributed").get('ngpus'), args=(args,))
+    else:
+        training_setup(None, args)
 
-    # OS Setup
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    os.environ['WORLD_SIZE'] = str(num_gpus)
-    mp.spawn(dist_train, nprocs=num_gpus, args=(args, num_gpus))
 
 
 
--- DeepLabV3/utils.py
+++ DeepLabV3/utils.py
@@ -6,13 +6,51 @@ import torch
 import os
 from dataset import PlanesDataset
 from torch.utils.data import DataLoader
-from torchvision.models.segmentation import fcn_resnet101
+from torchvision.models.segmentation import deeplabv3_resnet101
 from torch.nn.parallel import DistributedDataParallel as DDP, DataParallel as DP
 import torch.distributed as dist
 import numpy
 import random
 
-def augmentations():
+def read_config_file():
+    cla = _command_line_args()
+    with open(cla.config_file, "r") as stream:
+        return yaml.safe_load(stream)
+
+
+def _command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="path to config file")
+    args = parser.parse_args()
+    return args
+
+
+def dist_env_setup(args):
+    if args.get("distributed").get('ngpus') is None:
+        args['distributed']['ngpus'] = torch.cuda.device_count()
+    args['distributed']['world-size'] = args.get("distributed").get('nodes') * args.get("distributed").get('ngpus')
+    os.environ['MASTER_ADDR'] = args.get("distributed").get('ip-address')
+    os.environ['MASTER_PORT'] = '12355'
+    os.environ['WORLD_SIZE'] = str(args.get("distributed").get('world-size'))
+
+
+def dist_process_setup(args, gpu):
+    args['distributed']['gpu'] = gpu
+    dist_args = args.get('distributed')
+    rank = dist_args.get('local-ranks') * dist_args.get('ngpus') + gpu
+
+    dist.init_process_group(
+        backend='nccl',
+        world_size=dist_args.get('world-size'),
+        rank=rank,
+    )
+
+    torch.cuda.set_device(dist_args.get('gpu'))
+
+    return rank
+
+
+def _augmentations():
     train_transforms = A.Compose([
         A.Rotate(limit=35, p=1),
         A.HorizontalFlip(p=0.5),
@@ -33,7 +71,29 @@ def augmentations():
     return train_transforms, test_transforms
 
 
-def my_collate_fn(batch):
+def _get_datasets(args):
+    data_dir = args.get('config').get('data-dir')
+
+    img_dir = os.path.join(data_dir, 'train/images_tiled')
+    mask_dir = os.path.join(data_dir, 'train/masks_tiled')
+    test_img_dir = os.path.join(data_dir, 'test/images_tiled')
+    test_mask_dir = os.path.join(data_dir, 'test/masks_tiled')
+
+    train_transform, test_transform = _augmentations()
+
+    train_dataset = PlanesDataset(
+        img_dir=img_dir, mask_dir=mask_dir,
+        num_classes=args.get('config').get('classes'), transforms=train_transform,
+    )
+    test_dataset = PlanesDataset(
+        img_dir=test_img_dir, mask_dir=test_mask_dir,
+        num_classes=args.get('config').get('classes'), transforms=test_transform,
+    )
+
+    return train_dataset, test_dataset
+
+
+def _collate_fn(batch):
     images, labels = [], []
     for img, mask in batch:
         images.append(img)
@@ -41,3 +101,89 @@ def my_collate_fn(batch):
     images = torch.stack(images)
     labels = torch.stack(labels)
     return images, labels
+
+
+def _seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2 ** 32
+    numpy.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+def get_data_loaders(args, rank=None):
+    train_dataset, test_dataset = _get_datasets(args)
+
+    dist_args, hyper_params = args.get('distributed'), args.get('hyper-params')
+
+    train_sampler, test_sampler, batch_size = None, None, hyper_params.get('batch-size')
+    if args.get('config').get('distributed'):
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+            train_dataset, num_replicas=dist_args.get('world-size'), rank=rank
+        )
+        test_sampler = torch.utils.data.distributed.DistributedSampler(
+            test_dataset, num_replicas=dist_args.get('world-size'), rank=rank
+        )
+        batch_size = int(hyper_params.get('batch-size') / dist_args.get('ngpus'))
+
+    g = torch.Generator()
+    g.manual_seed(0)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=(train_sampler is None),
+        num_workers=hyper_params.get('workers'),
+        drop_last=True,
+        collate_fn=_collate_fn,
+        pin_memory=True,
+        sampler=train_sampler,
+        worker_init_fn=_seed_worker,
+        generator=g,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        num_workers=hyper_params.get('workers'),
+        collate_fn=_collate_fn,
+        pin_memory=True,
+        sampler=test_sampler,
+        worker_init_fn=_seed_worker,
+        generator=g,
+    )
+
+    return train_loader, test_loader
+
+
+def get_memory_format(args):
+    if args.get('config').get('channels-last'):
+        return torch.channels_last
+    else:
+        return torch.contiguous_format
+
+
+def get_device(args):
+    if args.get('config').get('distributed'):
+        gpu = args.get('distributed').get('gpu')
+        device = f'cuda:{gpu}'
+    else:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    return device
+
+
+def get_model(args):
+    if args.get('config').get('distributed'):
+        dist_args = args.get('distributed')
+        model = deeplabv3_resnet101(num_classes=args.get('config').get('classes')).cuda(dist_args.get('gpu'))
+        model = DDP(model, device_ids=[dist_args.get('gpu')])
+    else:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device_ids = [i for i in range(torch.cuda.device_count())]
+        model = deeplabv3_resnet101(num_classes=args.get('config').get('classes')).to(device)
+        model = DP(model, device_ids=device_ids)
+
+    model = model.to(memory_format=get_memory_format(args))
+
+    return model
+
+
+def is_main_node(rank):
+    return rank is None or rank == 0
